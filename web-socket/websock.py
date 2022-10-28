a1e378eaad3917083896c14182ea53c08c9ce487
Anmol Sandhu <anmolrattansandhu@gmail.com>
2022-10-27 21:48:53 -0400

    connected static files to flask app

--- web-socket/websock.py
+++ web-socket/websock.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template
 from flask_sock import Sock
+import json
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../web-ui/build', static_url_path='/')
 sock = Sock(app)
 
 
-@app.route("/")
-def test():
-    return None
-
-
-@sock.route('/echo')
+@sock.route('/socket')
 def echo(sock):
+    msg = json.dumps({
+        'action': 'update_score',
+        'score': 10
+    })
+    sock.send(msg)
 
     while True:
 
         message = sock.receive()
 
         sock.send(message + ' from server')
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file("index.html")
 
 
 if __name__ == "__main__":
