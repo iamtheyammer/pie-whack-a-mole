d35dc320a59246d213c22d293f157a7fec557eeb
Anmol Sandhu <anmolrattansandhu@gmail.com>
2022-11-29 11:29:32 -0500

    gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# pie-whack-a-mole
- 
--- pi-scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# adapted from https://dustinpfister.github.io/2020/03/27/linux-raspbian-lite-xserver-xorg/
-
-sudo apt install chromium xserver-xorg xinit x11-xserver-utils
-
-# ensure that SSH, I2C, autologin is enabled
